c3f11094371886b208511c00c1a217356aaf556b
hector810719 <a0987857017@gmail.com>
2022-05-12 19:34:59 +0800

    20220512, No.167 added Two pointer to solve question.

--- 167.Two Sum II -Input Array is sorted.py
+++ 167.Two Sum II -Input Array is sorted.py
@@ -4,11 +4,24 @@ def twoSum(numbers, target):
     :type target: int
     :rtype: List[int]
     """
+    # Make a dictionary to find which number you want.
     find_dict = {}
     for i in range(0, len(numbers_test)):
         if numbers_test[i] not in find_dict:
             find_dict[target_test - numbers_test[i]] = i
         print(find_dict[numbers_test[i]]+1, i+1)
+
+    # Two pointer to solve this question.
+    # l = 0
+    # r = len(numbers) - 1
+    # while numbers[l] + numbers[r] != target:
+    #     if numbers[l] + numbers[r] > target:
+    #         r -= 1
+    #     elif numbers[l] + numbers[r] < target:
+    #         l += 1
+    # return [l + 1, r + 1]
+    #
+
 # numbers_test = [2, 7, 11, 15]
 # numbers_test = [-1, 0]
 numbers_test = [0, 0, 3, 4]
